785ca096ec86bf18cd9032df73d3e75d4a69a6b3
MaxvanHaastrecht <65038675+MaxvanHaastrecht@users.noreply.github.com>
2020-07-13 13:45:50 +0200

    Comment edits for StreetMap.py

--- StreetMap.py
+++ StreetMap.py
@@ -2,11 +2,10 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-# Import roads.shp file, .shx file should also be contained in directory
+# Import roads.shp file; the corresponding .shx file should also be contained in the directory.
 map_df = gpd.read_file('roads.shp')
 
-# Define latitude-longitude pairs for different cities
-# Ensure grid sizes are the same
+# Define latitude-longitude pairs for different cities.
 x_diff = 0.0350
 y_diff = 0.0495
 latlon = {}
@@ -15,14 +14,14 @@ latlon['Groningen'] = [6.5480, 6.5480 + x_diff, 53.1960, 53.1960 + y_diff]
 latlon['Utrecht'] = [5.1050, 5.1050  + x_diff, 52.0620, 52.0620 + y_diff]
 latlon['Zwolle'] = [6.0730, 6.0730  + x_diff, 52.4900, 52.4900 + y_diff]
 
-# Store color palettes for cities
+# Store color palettes for cities.
 colors = {}
 colors['Doetinchem'] = 'summer'
 colors['Groningen'] = 'autumn'
 colors['Utrecht'] = 'Wistia'
 colors['Zwolle'] = 'winter'
 
-# Plot roads in particular city of NL
+# Plot roads in particular city of NL.
 city = 'Zwolle'
 
 fig, ax = plt.subplots(1, figsize=(10,14))
